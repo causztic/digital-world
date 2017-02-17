599d9d77acb6e08def6a1eda8f33174670f1f56e
IvanLowLLH <ivanlowlh@gmail.com>
2017-02-17 12:56:25 +0800

    add files week 4

--- mini_projects/wk4_ebot.py
+++ mini_projects/wk4_ebot.py
@@ -2,8 +2,8 @@ from eBot import eBot
 from time import sleep
 from firebase import firebase
 
-url = "Replace me" # URL to Firebase database
-token = "Replace me" # unique token used for authentication
+url = "https://ebot-ac352.firebaseio.com/" # URL to Firebase database
+token = "QwU8aMuyxCQk3lNH3Lh66chvmeMjnv7kRuixeTQB" # unique token used for authentication
 
 # Create a firebase object by specifying the URL of the database and its secret token.
 # The firebase object has functions put and get, that allows user to put data onto 
@@ -17,6 +17,7 @@ ebot.connect() # connect to the eBot via Bluetooth
 # the Firebase database.
 no_commands = True
 
+movement_dict = {'up':[1,1],'left':[1,0],'right':[0,1],'ok':[0,0]}
 while no_commands:
     # Check the value of movement_list in the database at an interval of 0.5
     # seconds. Continue checking as long as the movement_list is not in the
@@ -24,15 +25,25 @@ while no_commands:
     # exits the while loop and controls the eBot to perform the movements
     # specified in the movement_list in sequential order. Each movement in the
     # list lasts exactly 1 second.
-
-    # Get movement list from Firebase
-    movement_list = None
-
-    # Write your code here
-
     
-    sleep(0.5)
-
+    # Get movement list from Firebase
+    #movement_list = None
+    movement_list = firebase.get('/movement')
+    if movement_list == None:
+        ebot.wheels(0,0)
+    else:
+        for i in movement_list:
+            #to_do = movement_list[i]
+            #print to_do
+            #to_do = str(to_do)
+            print i
+            to_do_matrix = movement_dict[i]
+            ebot.wheels(to_do_matrix[0],to_do_matrix[1])
+            sleep(1)
+        firebase.put('/','movement',None)
+        print "Ez Game"
+    #sleep(0.5)
+    
 # Write the code to control the eBot here
 
 
--- mini_projects/wk4_raspberrypi.py
+++ mini_projects/wk4_raspberrypi.py
@@ -2,8 +2,8 @@ import RPi.GPIO as GPIO
 from time import sleep
 from firebase import firebase
 
-url = "Replace me" # URL to Firebase database
-token = "Replace me" # unique token used for authentication
+url = "https://ebot-ac352.firebaseio.com/" # URL to Firebase database
+token = "QwU8aMuyxCQk3lNH3Lh66chvmeMjnv7kRuixeTQB" # unique token used for authentication
 
 # Create a firebase object by specifying the URL of the database and its secret token.
 # The firebase object has functions put and get, that allows user to put data onto 
@@ -29,7 +29,20 @@ while not done:
     # the OK button is pressed, the program exits the while loop and writes the
     # movement_list to the Firebase database. If any of the directional buttons
     # are pressed, the commands should be stored in the movement_list.
-
+    if buttons['left'] == GPIO.High:
+        movement_list.append('left')
+        print 'left'
+    if buttons['up'] == GPIO.High:
+        movement_list.append('up')
+        print 'up'
+    if buttons['right'] == GPIO.High:
+        movement_list.append('right')
+        print 'right'
+    if buttons['ok'] == GPIO.High:
+        print 'Execute'
+        firebase.put('/','movement',movement_list)
+        movement_list = []
+    sleep(0.1)
     # Write your code here
     pass
 
