bc6e19987ee3d1c179a5437c2106c7e207a6e9b1
yaojie <limyaojie93@gmail.com>
2017-04-04 17:28:55 +0800

    update code

--- cobra/1D Kivy/button_test.py
+++ cobra/1D Kivy/button_test.py
@@ -4,45 +4,38 @@ from kivy.properties import StringProperty
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 
-
-class Milk(Widget):
+class GroceryItem(Widget):
     label_text = StringProperty()
 
     def __init__(self,**kwargs):
-        super(Milk,self).__init__(**kwargs)
-        self.count = 0
-        self.label_text = str(self.count)
-
-    def increment(self,*args):
+        super(GroceryItem, self).__init__(**kwargs)
+        acceptable_keys_list = ["count", "label_text", "name", "brand"]
+        for k in kwargs.keys():
+            if k in [acceptable_keys_list]:
+                self.__setattr__(k, kwargs[k])
+        
+        # add image to GroceryItem
+        # add + and - button to GroceryItem
+        # add callbacks to GroceryItem
+    
+    def increment(self):
         self.count += 1
         self.label_text = str(self.count)
 
-    def decrement(self,*args):
+    def decrement(self):
         self.count -= 1
         self.label_text = str(self.count)
-
-class Coke(Widget):
-    label_text = StringProperty()
-
-    def __init__(self,**kwargs):
-        super(Coke,self).__init__(**kwargs)
-        self.count = 0
-        self.label_text = str(self.count)
-
-    def increment(self,*args):
-        self.count += 1
-        self.label_text = str(self.count)
-
-    def decrement(self,*args):
-        self.count -= 1
-        self.label_text = str(self.count)
-
-class Items(Widget):
-    pass
-
+    
 class ButApp(App):
     
     def build(self):
-        return Items()
+        # import gridview here
+        list_of_groceries = [{"name": "milk", "count": 5}, { "name": "coke", "count": "3"}]
+        for grocery in list_of_groceries:
+            GroceryItem(grocery)
+            # for every item, add to gridview
+        
+        #return the gridview with the groceryitems
+        return None
 
 ButApp().run()
