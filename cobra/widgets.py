34fac3eeeb96c6c9e96d0b9108106195a937de35
yaojie <limyaojie93@gmail.com>
2017-04-05 14:32:34 +0800

    update widgets

--- cobra/widgets.py
+++ cobra/widgets.py
@@ -1,4 +1,6 @@
 from kivy.uix.widget import Widget
+from kivy.uix.image  import Image
+from kivy.uix.button import Button
 from kivy.properties import StringProperty
 
 class GroceryItem(Widget):
@@ -6,14 +8,11 @@ class GroceryItem(Widget):
 
     def __init__(self,**kwargs):
         super(GroceryItem, self).__init__(**kwargs)
+        self.count = 0
         acceptable_keys_list = ["count", "label_text", "name", "brand"]
         for k in kwargs.keys():
             if k in [acceptable_keys_list]:
                 self.__setattr__(k, kwargs[k])
-        
-        # add image to GroceryItem
-        # add + and - button to GroceryItem
-        # add callbacks to GroceryItem
     
     def increment(self):
         self.count += 1
@@ -22,3 +21,12 @@ class GroceryItem(Widget):
     def decrement(self):
         self.count -= 1
         self.label_text = str(self.count)
+
+    def build(self):
+        image = Image(size=(300,300), source=self.name+".png")
+        add_button = Button(size=(50,50), pos=(image.center_x+100, image.center_y-100), text="+", on_press=(self.increment()))
+        remove_button = Button(size=(50,50), pos=(image.center_x+100, image.center_y-100), text="+", on_press=(self.decrement()))
+        image.add_widget(add_button)
+        image.add_widget(remove_button)
+        self.add_widget(image)
+        return self
