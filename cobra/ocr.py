dfb317755bbc1e63445f7677047d51107642873c
yaojie <limyaojie93@gmail.com>
2017-04-05 11:16:15 +0800

    update crude ocr

--- cobra/ocr.py
+++ cobra/ocr.py
@@ -1,4 +1,16 @@
+from io import BytesIO
+from time import sleep
+from picamera import PiCamera
 from PIL import Image
 import pytesseract
 
-print(pytesseract.image_to_string(Image.open('cobra/ocr_test.jpg')))
+
+# Create the in-memory stream
+stream = BytesIO()
+camera = PiCamera()
+camera.start_preview()
+sleep(2)
+camera.capture(stream, format='jpeg')
+# "Rewind" the stream to the beginning so we can read its content
+stream.seek(0)
+print(pytesseract.image_to_string(Image.open(stream)))
