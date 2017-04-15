e94b953fe360117c73c657935e55a4b7f102749f
IvanLowLLH <ivanlowlh@gmail.com>
2017-04-15 16:53:18 +0800

    changed cobra.kv and widget.kv

--- cobra/cobra.py
+++ cobra/cobra.py
@@ -1,16 +1,40 @@
 from kivy.app import App
+from kivy.uix.image  import Image
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.boxlayout import BoxLayout
 
-from ocr import OCR
+#from ocr import OCR
 from widgets import GroceryItem
+
+list_items = ['milk','apple','chocolate','sof drinks','shrimp','steak','meat','broccoli']
+
+class InventoryScreen(Screen):
+    def __init__(self, **kwargs):
+        Screen.__init__(self, **kwargs)
+        overall_layout = BoxLayout(orientation = 'vertical')
+        topbox = BoxLayout(orientation='horizontal')
+        label = Label(text='Inventory',color = (0,0,0,1),font_size=60)
+        camera = Image(source = 'camera.png')
+        topbox.add_widget(label)
+        topbox.add_widget(camera)
+        overall_layout.add_widget(topbox)
+        bottom_layout = GridLayout(cols=3)
+        for items in list_items:
+            bottom_layout.add_widget(GroceryItem(name=items))
+        overall_layout.add_widget(bottom_layout)
+        self.add_widget(overall_layout)
+        
 
 class CobraApp(App):
 
     def build(self):
-        layout = GridLayout(cols=3)
-        layout.add_widget(GroceryItem(name="milk"))
-        return layout
+        sm=ScreenManager()
+        i_s = InventoryScreen(name='Inventory')
+        sm.add_widget(i_s)
+        return sm
 
 if __name__ == '__main__':
     CobraApp().run()
--- cobra/widgets.py
+++ cobra/widgets.py
@@ -2,9 +2,11 @@ from kivy.uix.widget import Widget
 from kivy.uix.image  import Image
 from kivy.uix.button import Button
 from kivy.properties import StringProperty
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+
 
 class GroceryItem(Widget):
-    label_text = StringProperty()
 
     def __init__(self,**kwargs):
         super(GroceryItem, self).__init__(**kwargs)
@@ -13,17 +15,28 @@ class GroceryItem(Widget):
         for k in kwargs.keys():
             if k in acceptable_keys_list:
                 self.__setattr__(k, kwargs[k])
-        image = Image(size=(300,300), source=self.name+".png")
-        add_button = Button(size=(50,50), pos=(image.center_x+100, image.center_y-100), text="+")
-        remove_button = Button(size=(50,50), pos=(image.center_x+100, image.center_y-100), text="-")
+        image = Image(size=(200,200), source=self.name+".png")
+        add_button = Button(size=(25,25), pos=(image.center_x+100, image.center_y), text="+")
+        plus_sign = Image(size = (50,50),pos = (add_button.center_x-25,add_button.center_y-25),source = 'plus.png')
+        add_button.add_widget(plus_sign)
+        remove_button = Button(size=(25,25), pos=(image.center_x+100, image.center_y-75), text="-")
+        minus_sign = Image(size = (50,50),pos = (remove_button.center_x-25,remove_button.center_y-25),source = 'minus.png')
+        remove_button.add_widget(minus_sign)
+        counter_backgrnd = Image (size=(50,50),source = 'label.png',pos =(image.center_x-100, image.center_y-75))
+        self.counter = Label(size=(20,20),pos=(counter_backgrnd.center_x-9, counter_backgrnd.center_y-6.25),text='0',font_size = 30, color=(1,1,1,1))
+        counter_backgrnd.add_widget(self.counter)
         image.add_widget(add_button)
         image.add_widget(remove_button)
+        image.add_widget(counter_backgrnd)
         self.add_widget(image)
+        image.size_hint = (None,None)
+        add_button.bind(on_press = self.increment)
+        remove_button.bind(on_press = self.decrement)
 
-    def increment(self):
+    def increment(self,instance):
         self.count += 1
-        self.label_text = str(self.count)
+        self.counter.text = str(self.count)
 
-    def decrement(self):
+    def decrement(self,instance):
         self.count -= 1
-        self.label_text = str(self.count)
+        self.counter.text = str(self.count)
