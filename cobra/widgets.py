28ee3d01020446423087ccd612dd7a12f4f4937d
yaojie <limyaojie93@gmail.com>
2017-04-18 15:13:49 +0800

    update widgets

--- cobra/widgets.py
+++ cobra/widgets.py
@@ -58,14 +58,13 @@ class CamItem(BoxLayout):
     def __init__(self, **kwargs):
         super(CamItem, self).__init__(**kwargs)
         self.stream = BytesIO()
-        self.camera = Camera(resolution=(800,600), play=False)
+        self.camera = Camera(play=False)
 
         self.take_photo_button = Button(text="Analyze Receipt", on_press=self.analyze_photo)
         self.activate_button = ToggleButton(text="Toggle Camera", on_press=self.toggle_camera)
         
         self.add_widget(self.camera)
         self.add_widget(self.activate_button)
-        self.add_widget(self.take_photo_button)
 
     def toggle_camera(self, instance):
         self.camera.play = not self.camera.play
