6cb204b1b5f6c8609a906bea8ba4e8f958fee1e0
yaojie <limyaojie93@gmail.com>
2017-04-20 15:32:10 +0800

    fix syntax error

--- cobra/widgets.py
+++ cobra/widgets.py
@@ -161,14 +161,9 @@ class RawKivyCamera(Image):
             gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
             (thresh, bw_img) = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
             txt = pytesseract.image_to_string(pil_image.fromarray(bw_img))
-            choices = { "milk": ["HL", "Milk"], 
-                        "chocolate": ["Crunchie", "Hershey"], 
-                        "apple": ["Apple", "Fuji Apple"],
-                        "broccoli": ["Broccoli"], 
-                        "chicken": ["Chicken"], 
-                        "cola": ["Coca-Cola"] }
+            choices = { "milk": ["HL", "Milk"], "chocolate": ["Crunchie", "Hershey"], "apple": ["Apple", "Fuji Apple"], "broccoli": ["Broccoli"],  "chicken": ["Chicken"], "cola": ["Coca-Cola"] }
             for line in txt:
-                all_values = [item for sublist in choices.values for item in sublist]
+                all_values = [item for sublist in choices.values() for item in sublist]
                 value, score = process.extractOne(line, all_values)
                 for k, v in choices.iteritems():
                     if value in v:
