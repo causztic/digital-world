5ca55693ba3ff1c205ce04b9e4ed4e7953cd2c7e
yaojie <limyaojie93@gmail.com>
2017-04-20 13:43:51 +0800

    + shape detection

--- cobra/widgets.py
+++ cobra/widgets.py
@@ -64,6 +64,44 @@ class GroceryItem(RelativeLayout):
         self.counter.text = str(self.count)
         firebase.put('/',self.name,self.counter.text)
 
+
+class ShapeDetector(object):
+	def __init__(self):
+		pass
+
+	def detect(self, c):
+		# initialize the shape name and approximate the contour
+		shape = "unidentified"
+		peri = cv2.arcLength(c, True)
+		approx = cv2.approxPolyDP(c, 0.04 * peri, True)
+
+		# if the shape is a triangle, it will have 3 vertices
+		if len(approx) == 3:
+			shape = "triangle"
+
+		# if the shape has 4 vertices, it is either a square or
+		# a rectangle
+		elif len(approx) == 4:
+			# compute the bounding box of the contour and use the
+			# bounding box to compute the aspect ratio
+			(x, y, w, h) = cv2.boundingRect(approx)
+			ar = w / float(h)
+
+			# a square will have an aspect ratio that is approximately
+			# equal to one, otherwise, the shape is a rectangle
+			shape = "square" if ar >= 0.95 and ar <= 1.05 else "rectangle"
+
+		# if the shape is a pentagon, it will have 5 vertices
+		elif len(approx) == 5:
+			shape = "pentagon"
+
+		# otherwise, we assume the shape is a circle
+		else:
+			shape = "circle"
+
+		# return the name of the shape
+		return shape
+
 class RawKivyCamera(Image):
     def __init__(self, capture, fps, play = False, **kwargs):
         super(RawKivyCamera, self).__init__(**kwargs)
@@ -80,12 +118,40 @@ class RawKivyCamera(Image):
             if ret:
                 # convert the resized image to grayscale, blur it slightly,
                 # and threshold it
-                self.buffer = frame
+                self.buffer = self.detect_shapes(frame)
                 self.rgb = cv2.cvtColor(self.buffer, cv2.COLOR_BGR2RGB)
                 self.texture.blit_buffer(self.rgb.tostring(), colorfmt='rgb', bufferfmt='ubyte')
                 self.canvas.ask_update()
                 self.buffer = None
+    
+    def detect_shapes(self, frame):
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+        thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
+        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+        cv2.CHAIN_APPROX_SIMPLE)
+        cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+        sd = ShapeDetector()
 
+        for c in cnts:
+            # compute the center of the contour, then detect the name of the
+            # shape using only the contour
+            M = cv2.moments(c)
+            cX = int((M["m10"] / (M["m00"] + 1e-7)))
+            cY = int((M["m01"] / (M["m00"] + 1e-7)))
+            shape = sd.detect(c)
+
+            # multiply the contour (x, y)-coordinates by the resize ratio,
+            # then draw the contours and the name of the shape on the image
+            c = c.astype("float")
+            c = c.astype("int")
+            cv2.drawContours(frame, [c], -1, (0, 255, 0), 2)
+            cv2.putText(frame, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
+                0.5, (255, 255, 255), 2)
+        
+        return frame
+
+    
     def analyze_photo(self, instance):
         if self.rgb is not None:
             cv2.imwrite("test.png", self.rgb)
